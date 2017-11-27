a41df1509a2ee9b3790c7642706d8d8f68276c17
vimalpatel87 <vimal.161187@gmail.com>
2017-11-27 12:27:04 -0600

    Library App
    Library App Developed

---
+++ js/app.js
@@ -0,0 +1,41 @@
+var libraryApp = angular.module('libraryApp',[]);
+libraryApp.controller('libraryController', function($scope){
+    $scope.myBooks = [];
+    $scope.booksResults = [];
+    
+    //Add Book to Library
+    $scope.addToLibrary = function(Book){
+           $scope.myBooks.push(Book);
+    }
+    
+    //Check if Book is already added in Library
+    $scope.isBookadded = function(Book){
+    if(angular.toJson($scope.myBooks).includes(angular.toJson(Book))) {  
+        return true;
+    } else {
+        return false;
+    }};
+    
+    var api_prefix = "https://www.googleapis.com/books/v1/volumes?q=";
+
+    //Search for Books from Google API
+    $scope.searchBooks = function(key) {
+        $("#loading-image").show();
+        var request = api_prefix + key;
+        $.get(request, function(resp) {
+            if(resp.items && resp.items.length > 0) {
+                $scope.booksResults = resp.items;
+                $scope.$apply();
+                $("#loading-image").hide();
+            }
+            else {
+                alert('Sorry, No Book Found!');
+                $("#loading-image").hide();
+            }  
+        })
+        .error(function(e){
+            console.log(e);
+            $("#loading-image").hide();
+        });
+  }
+});
